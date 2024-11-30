adfde3c1ef976de35fad9b4087cd56f823a2c35e
Azakana <danielsaputradivo@gmail.com>
2024-11-30 13:18:54 +0700

    .

--- Install.sh
+++ Install.sh
@@ -6,8 +6,8 @@ echo -e "\033[31m    █████╔╝ ███████║██║    
 echo "    ██╔═██╗ ██╔══██║██║     ██║██║╚██╔╝██║██║   ██║ ██╔██╗ "
 echo "    ██║  ██╗██║  ██║███████╗██║██║ ╚═╝ ██║╚██████╔╝██╔╝ ██╗"
 echo "    ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝"
-echo -e "\033[31m                      Code By: J3sterr\033[0m"
-echo -e "\033[38;2;0;255;0m                Team:BojonegoroDarknetSociety\033[0m"
+echo -e "\033[31m                       Code By: J3sterr\033[0m"
+echo -e "\033[38;2;0;255;0m                Team:Bojonegoro Darknet Society\033[0m"
 
 #! Default Directory
 install_dir="/data/data/com.termux/files/home/"
